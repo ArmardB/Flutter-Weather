e7c045d6a299c3579d53591ea5f37d741cba2385
Armard Bellamy <armardbellamy@gmail.com>
2018-09-22 19:43:01 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ui/Weather.dart';
+
+void main() {
+  runApp(
+      new MaterialApp(
+        title: 'Flutter Weather',
+        home: new Weather(),
+    )
+  );
+}
---
+++ lib/ui/Weather.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new WeatherState();
+  }
+
+}
+
+
+class WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+
+}
